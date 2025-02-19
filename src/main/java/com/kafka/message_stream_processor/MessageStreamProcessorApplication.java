d15b73a1a2b796c015d382edb238af05d07caa9a
felixojiambo <felixojiamboe@gmail.com>
2025-02-19 16:34:37 +0300

    initial commit

---
+++ src/main/java/com/kafka/message_stream_processor/MessageStreamProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.message_stream_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStreamProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStreamProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/message_stream_processor/MessageStreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.message_stream_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageStreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
